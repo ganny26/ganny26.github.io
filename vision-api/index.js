eff6b935e202fdee24589dbe0e12d11645b466cf
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-06-27 10:33:56 +0530

    hbs view added

--- vision-api/index.js
+++ vision-api/index.js
@@ -1,57 +1,30 @@
 const express = require('express');
-const vision = require('@google-cloud/vision');
-
-const projectId = 'oao-india';
+const async = require('async');
+const fs = require('fs');
+const multer  = require('multer');
+var exphbs   =  require( 'express-handlebars' );
+const visionClient = require('./VisionClient');
 
 var app = express();
 
-/**Google Vision Client */
-const visionClient = vision({
-    projectId: projectId,
-    keyFilename: 'application_default_credentials.json'
+app.engine( '.hbs', exphbs( { extname: '.hbs' } ) );
+
+app.set('view engine', '.hbs');
+app.set('views', __dirname + '/views');
+
+var upload = multer({ dest: 'uploads/' });
+
+
+app.get('/', function(req, res,next) {
+  //res.render( 'index' );
+  visionClient.scan();
 });
 
 
-function scan(req, res) {
-    var passportNumber;
-    var status;
-    visionClient.detectText('img/passport2.jpg')
-        .then((results) => {
-            const detections = (results[0] + '').split('\n');
-            const regexPassport = '^([A-Z]{1})([0-9]{7})$';
-            for (var i = 0; i < detections.length; i++) {
-                var validPassport = detections[i].replace(/\s/g, '');
-                console.log(validPassport);
-                if (validPassport.match(regexPassport)) {
-                    console.log('matched');
-                    passportNumber = validPassport;
-                    status = true;
-                    break;
-                } else {
-                    passportNumber = "not found";
-                    status = false;
-                    console.log('Error');
-                }
-            };
-
-
-            res.json({
-                success: status,
-                payload: passportNumber
-            });
-        }).catch(function (err) {
-            console.log('vision api error', err)
-            res.json({
-                success: false,
-                payload: 'internal server error'
-            });
-        });
-}
-
-app.get('/', function (req, res) {
-    scan(req, res);
-});
-
-app.listen(8656,function(req,res){
-    console.log('app running on localhost:8656');
+app.post('/fileupload',upload.single('avatar'),function(req,res,next){
+    console.log('calling file upload');
+    console.log('File >>>',req.file);
+})
+app.listen(8656,function(){
+    console.log("server running on http://localhost:8656");
 });
