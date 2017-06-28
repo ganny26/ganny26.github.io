1a91f0df730f132a20ce7e8875788e1334f17082
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-06-28 11:00:29 +0530

    jump npm added

--- vision-api/index.js
+++ vision-api/index.js
@@ -16,14 +16,24 @@ var upload = multer({ dest: 'uploads/' });
 
 
 app.get('/', function(req, res,next) {
-  //res.render( 'index' );
-  visionClient.scan();
+  visionClient.scan(req,res,function(result){
+     var scanData = {
+                "status":true,
+                "payload":{
+                    "name":result[3],
+                    "license_number":result[5],
+                    "license_expiry":result[10],
+                    "dob":result[15]
+                }
+            }
+        res.send(scanData);
+  });
 });
 
 
 app.post('/fileupload',upload.single('avatar'),function(req,res,next){
     console.log('calling file upload');
-    console.log('File >>>',req.file);
+    console.log('file uploaded succcesfully',req.file);
 })
 app.listen(8656,function(){
     console.log("server running on http://localhost:8656");
