cab2b32bc7666b10c6dbbc8573d4368b13ef837f
selvaganeshr <selvaganeshrajam@gmail.com>
2018-01-11 23:32:34 +0530

    converted to react js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import BannerComponent from './component/BannerComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+       <BannerComponent/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/BannerComponent.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import SocialIcon from '../component/SocialIcon';
+class BannerComponent extends Component {
+    render() {
+        return (
+            <div className="banner">
+                <h1>Selvaganesh Rajam</h1>
+                <h5 className="banner__description">JavaScript lover, React JS & Android Developer ,
+         Open Source enthusiast</h5>
+                <div className="banner__contact">
+                    <a href="/resume">
+                        <button className="c_button">Resume</button>
+                    </a>
+                    <a href="mailto:selvaganeshrajam@gmail.com">
+                        <button className="c_button">Contact</button>
+                    </a>
+                    <div className="contact">
+                        <SocialIcon iconname="fab fa-github" url="https://github.com/ganny26/" />
+                        <SocialIcon iconname="fab fa-medium-m" url="https://medium.com/@selvaganesh93/" />
+                        <SocialIcon iconname="fab fa-twitter" url="https://twitter.com/seganesa" />
+                        <SocialIcon iconname="fab fa-linkedin-in" url="https://in.linkedin.com/in/selvaganesh-rajam" />
+                        <SocialIcon iconname="fab fa-instagram" url="https://www.instagram.com/selvaganesh93/" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BannerComponent;
