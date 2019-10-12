d89027c60463765559679391484610e6ad1da156
Selvaganesh <selvaganeshrajam@gmail.com>
2019-10-12 19:14:21 +0530

    Refactored

--- src/component/SocialIcon.js
+++ src/component/SocialIcon.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class SocialIcon extends Component {
-    constructor() {
-        super();
-        this.state = {
-            'iconName': '',
-            'linkurl': ''
-        }
-    }
-    componentWillMount() {
-        this.setState({
-            'iconName': this.props.iconname,
-            'linkurl': this.props.url
-        })
-    }
-    render() {
-        return (
-            <div className="circle-btn">
-                <a className='link-c' href={this.state.linkurl}>
-                    <span className={this.state.iconName} ></span>
-                </a>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      iconName: "",
+      linkurl: ""
+    };
+  }
+  componentWillMount() {
+    this.setState({
+      iconName: this.props.iconname,
+      linkurl: this.props.url
+    });
+  }
+  render() {
+    return (
+      <div className="circle-btn">
+        <a className="link-c" href={this.state.linkurl}>
+          <span className={this.state.iconName}></span>
+        </a>
+      </div>
+    );
+  }
 }
 
 export default SocialIcon;
